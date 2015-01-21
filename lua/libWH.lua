09b5468d6afd943c133d91a267c68ef24e5626a9
Alexandre Rio <contact@alexrio.fr>
2015-01-21 01:12:30 +0100

    add tree tools in libWH

--- lua/libWH.lua
+++ lua/libWH.lua
@@ -7,6 +7,22 @@ function libWH.isNil(expr)
   return expr.name == nil
 end
 
+function libWH.newLeaf()
+  local leaf = {}
+  leaf.name = "leaf"
+  --leave sons field to nil value
+  return leaf
+end
+
+function libWH.setLeaf(node, left, right)
+  node.name = nil
+  node.left = left
+  node.right = right
+end
+
+function libWH.setLeaf(node, left)
+  libWH.setLeaf(node, left, libWH.newLeaf())
+end
 
 function libWH.tree(treeName)
   local tree = {}
@@ -14,6 +30,16 @@ function libWH.tree(treeName)
   tree.left  = nil
   tree.right = nil
   return tree
+end
+
+function libWH.printTree(node)
+  print (node.name)
+  if node.left ~= nil then
+    libWH.printTree(node.left)
+  end
+  if node.right ~= nil then
+    libWH.printTree(node.right)
+  end
 end
 
 return libWH
