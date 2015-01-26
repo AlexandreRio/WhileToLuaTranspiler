f4fc1c167c37c21503da8699c9620d8a3beee81b
Alexandre Rio <contact@alexrio.fr>
2015-01-26 17:22:30 +0100

    don't run if only param is -h

--- org.xtext.tl.mydsl/src/org/xtext/tl/mydsl/WhC.java
+++ org.xtext.tl.mydsl/src/org/xtext/tl/mydsl/WhC.java
@@ -7,17 +7,24 @@ public class WhC {
 
   public static void main (String[] args) throws Exception {
     PrintStream output = System.out;
+    boolean run = true;
     if (args.length >= 1) {
       FrontEnd fe = new FrontEnd(args[0]);
       BackEnd be = new BackEnd(fe);
       if (args.length == 3 && args[1].equals("-o")) {
         output = new PrintStream(new File(args[2]));
+      } else if (args[0].equals("-h")){
+        run = false;
+        //display help
       } else {
         String defaultOutputName = be.getOutputFileName();
         output = new PrintStream(new File(defaultOutputName + ".lua"));
       }
-      String program = be.run();
-      output.println(program);
+
+      if (run) {
+        String program = be.run();
+        output.println(program);
+      }
     }
   }
 }
